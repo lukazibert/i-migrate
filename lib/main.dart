d67f088648dd5172b649161140ef9f77125ce219
Luka Zibert <lukazibert@Lukas-MacBook-Pro.local>
2023-02-25 18:04:36 +0100

    Added navigation and started with maps view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'test.dart';
+import 'package:i_migrate/views/GoogleMapsView.dart';
+import 'package:i_migrate/views/GoogleMapsView.dart';
+import 'package:i_migrate/views/SelectionView.dart';
+import 'components/CustomAppBar.dart';
+import 'components/CustomBottomNavigationBar.dart';
+import 'dart:developer';
 
 void main() {
   runApp(const MyApp());
@@ -13,48 +18,121 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: SelectionView(),
+      home: Composition(),
     );
   }
 }
 
-class SelectionView extends StatelessWidget {
-  const SelectionView({super.key});
+class Composition extends StatefulWidget {
+  Composition({super.key});
+
+  @override
+  State<Composition> createState() => _CompositionState();
+}
+
+class _CompositionState extends State<Composition> {
+  int _currentPageIndex = 0;
+
+  final List<CustomAppBar?> _appBarList = [
+    null,
+    CustomAppBar(
+        title: "Selection",
+        leadingIcon: Container(
+          width: 28,
+          height: 28,
+        ),
+        tailIcon: IconButton(
+          icon: Image.asset(
+            'lib/assets/Icons/CompareIcon.png',
+          ),
+          onPressed: () {},
+          iconSize: 28.0,
+        )),
+    CustomAppBar(
+      title: "Migration Agent",
+      leadingIcon: Container(
+        width: 28,
+        height: 28,
+      ),
+      tailIcon: Container(
+        width: 28,
+        height: 28,
+      ),
+    ),
+    CustomAppBar(
+      title: "User Profile",
+      leadingIcon: Container(
+        width: 28,
+        height: 28,
+      ),
+      tailIcon: Container(
+        width: 28,
+        height: 28,
+      ),
+    )
+  ];
+
+  final PageController _pageController = PageController(initialPage: 0);
+  bool _disableScroll = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page == 0) {
+        setState(() {
+          _disableScroll = true;
+        });
+      } else {
+        setState(() {
+          _disableScroll = false;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text("Selection"),
+      appBar: _appBarList[_currentPageIndex],
+      body: PageView.builder(
+        physics: _disableScroll
+            ? NeverScrollableScrollPhysics()
+            : ClampingScrollPhysics(),
+        controller: _pageController,
+        itemCount: _appBarList.length,
+        itemBuilder: ((context, index) {
+          switch (index) {
+            case 0:
+              return GoogleMapsView();
+            case 1:
+              return SelectionView();
+            case 2:
+              return Container();
+            case 3:
+              return Container();
+            default:
+              return GoogleMapsView();
+          }
+        }),
+        onPageChanged: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
       ),
-      body: Container(
-        padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 5),
-        child: Column(children: [
-          Container(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Germany",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'Alegreya Sans',
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ),
-                  Icon(
-                    Icons.info_outline,
-                    color: Colors.grey,
-                  ),
-                ]),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Color(0xFFEFEFEF),
-            ),
-            padding: EdgeInsets.all(25),
-          ),
-        ]),
+      bottomNavigationBar: MyCustomBottomNavigtionBar(
+        currentIndex: _currentPageIndex,
+        onClick: (selectedPageIndex) => {
+          setState((() => {
+                _currentPageIndex = selectedPageIndex,
+                _pageController.animateToPage(
+                  selectedPageIndex,
+                  duration: Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                )
+              }))
+        },
       ),
     );
   }
